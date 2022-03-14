eda7620917aaa634434dc182987ba78c2786273c
Matija Pretnar <matija@pretnar.info>
2022-03-14 14:19:30 +0100

    Dodal mini lambdo

---
+++ 05-lambda-racun/lambda/parser.ml
@@ -0,0 +1,147 @@
+let explode str = List.init (String.length str) (String.get str)
+
+let implode chrs = String.init (List.length chrs) (List.nth chrs)
+
+type 'value parser = char list -> ('value * char list) option
+
+(* BASIC PARSERS *)
+
+let fail _ = None
+
+let return v chrs = Some (v, chrs)
+
+let character = function
+  | [] -> None
+  | chr :: chrs -> Some (chr, chrs)
+
+
+let ( || ) parser1 parser2 chrs =
+  match parser1 chrs with
+  | None -> parser2 chrs
+  | Some (v, chrs') -> Some (v, chrs')
+
+
+let ( >>= ) parser1 parser2 chrs =
+  match parser1 chrs with
+  | None -> None
+  | Some (v, chrs') -> parser2 v chrs'
+
+(* DERIVED PARSERS *)
+
+let ( >> ) parser1 parser2 =
+  parser1 >>= fun _ -> parser2
+
+let satisfy cond parser =
+  let cond_parser v =
+    if cond v then return v else fail in
+  parser >>= cond_parser
+
+let digit =
+  let is_digit = String.contains "0123456789" in
+  character |> satisfy is_digit
+
+let alpha =
+  let is_alpha = String.contains "_abcdefghijklmnopqrstvwuxyz" in
+  character |> satisfy is_alpha
+
+let space =
+  let is_space = String.contains " \n\t\r" in
+  character |> satisfy is_space
+
+let exactly chr = character |> satisfy (( = ) chr)
+
+let one_of parsers = List.fold_right ( || ) parsers fail
+
+let word str =
+  let chrs = explode str in
+  List.fold_right
+    (fun chr parser -> exactly chr >> parser)
+    chrs (return ())
+
+let rec many parser = many1 parser || return []
+
+and many1 parser =
+  parser >>= fun v ->
+  many parser >>= fun vs ->
+  return (v :: vs)
+
+let integer =
+  many1 digit >>= fun digits ->
+  return (int_of_string (implode digits))
+
+let spaces = many space >> return ()
+
+let spaces1 = many1 space >> return ()
+
+let parens parser =
+  word "(" >>
+  spaces >>
+  parser >>= fun p ->
+  spaces >>
+  word ")" >>
+  return p
+
+let binop parser1 op parser2 f =
+  parser1 >>= fun v1 ->
+  spaces >>
+  word op >>
+  spaces >>
+  parser2 >>= fun v2 ->
+  return (f v1 v2)
+
+(* LAMBDA PARSERS *)
+
+let ident =
+  alpha >>= fun chr ->
+  many (alpha || digit || exactly '\'') >>= fun chrs ->
+  return (Syntax.Ident (implode (chr :: chrs)))
+
+let rec exp2 chrs =
+  let lambda =
+    word "FUN" >>
+    spaces1 >>
+    ident >>= fun x ->
+    spaces1 >>
+    word "->" >>
+    spaces1 >>
+    exp2 >>= fun e ->
+    return (Syntax.Lambda (x, e))
+  and let_in =
+    word "LET" >>
+    spaces1 >>
+    ident >>= fun x ->
+    spaces >>
+    word "=" >>
+    spaces >>
+    exp2 >>= fun e1 ->
+    spaces1 >>
+    word "IN" >>
+    spaces1 >>
+    exp2 >>= fun e2 ->
+    return (Syntax.let_in (x, e1, e2))
+  in
+  one_of [lambda; let_in; exp1] chrs
+
+and exp1 chrs =
+  let apply =
+    exp0 >>= fun e ->
+    many1 (spaces1 >> exp0) >>= fun es ->
+    return (List.fold_left (fun e1 e2 -> Syntax.Apply (e1, e2)) e es)
+  in
+  one_of
+    [ apply
+    ; exp0 ]
+    chrs
+
+
+and exp0 chrs =
+  one_of
+    [ (ident >>= fun x -> return (Syntax.Var x))
+    ; parens exp2 ]
+    chrs
+
+
+let parse str =
+  match str |> String.trim |> explode |> exp2 with
+  | Some (v, []) -> v
+  | Some (_, _ :: _) | None -> failwith "Parsing error"
---
+++ 05-lambda-racun/lambda/syntax.ml
@@ -0,0 +1,36 @@
+type ident = Ident of string
+
+type exp =
+  | Var of ident
+  | Lambda of ident * exp
+  | Apply of exp * exp
+
+let let_in (x, e1, e2) = Apply (Lambda (x, e2), e1)
+let rec subst_exp sbst = function
+  | Var x as e ->
+      begin match List.assoc_opt x sbst with
+      | None -> e
+      | Some e' -> e'
+      end
+  | Lambda (x, e) ->
+      let sbst' = List.remove_assoc x sbst in
+      Lambda (x, subst_exp sbst' e)
+  | Apply (e1, e2) -> Apply (subst_exp sbst e1, subst_exp sbst e2)
+
+let string_of_ident (Ident x) = x
+
+let rec string_of_exp3 = function
+  | Lambda (x, e) ->
+      "FUN " ^ string_of_ident x ^ " -> " ^ string_of_exp3 e
+  | e -> string_of_exp2 e
+and string_of_exp2 = function
+  | e -> string_of_exp1 e
+and string_of_exp1 = function
+  | Apply (e1, e2) ->
+    string_of_exp0 e1 ^ " " ^ string_of_exp0 e2
+  | e -> string_of_exp0 e
+and string_of_exp0 = function
+  | Var x -> string_of_ident x
+  | e -> "(" ^ string_of_exp3 e ^ ")"
+
+let string_of_exp = string_of_exp3
